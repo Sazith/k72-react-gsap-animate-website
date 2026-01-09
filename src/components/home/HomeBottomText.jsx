5fab085a9229238cb1976cb6e712900e7b0eca5c
Sazith <sazith.simecsoftdev@gmail.com>
2026-01-09 21:41:54 +0600

    update home & install gsap

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom"
 
 const HomeBottomText = () => {
     return (
-        <div className="font-[lausanne-bold] flex items-center justify-center gap-2">
-            <Link className="text-[6.5vw] border-4 leading-[7vw] border-white rounded-full px-10 py-1 pt-1 pb-0 uppercase">Projects</Link>
-            <Link className="text-[6.5vw] border-4 leading-[7vw] border-white rounded-full px-10 py-1 pt-1 pb-0 uppercase">Agence</Link>
+      <div className="font-[lausanne-bold] flex items-center justify-center gap-6">
+        <div className="border-4 hover:border-[#D3FD50] hover:text-[#D3FD50] h-44 flex items-center px-10 border-white rounded-full uppercase">
+          <Link className="text-[6vw] mt-6 " to="/projects">
+            Projects
+          </Link>
         </div>
-    )
+        <div  className="border-4 h-44 flex items-center px-10 border-white rounded-full uppercase hover:border-[#D3FD50] hover:text-[#D3FD50]">
+          <Link className="text-[6vw] mt-6 " to="/agence">
+            Agence
+          </Link>
+        </div>
+      </div>
+    );
 }
 
 export default HomeBottomText
